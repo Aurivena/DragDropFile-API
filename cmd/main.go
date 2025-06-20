d540eeb351b34c5ca1312eb0aee0ef7f67cbc815
Konovalov Vlad <vladkonovalov20060@gmail.com>
2025-06-20 16:00:34 +0500

    логи

--- cmd/main.go
+++ cmd/main.go
@@ -48,6 +48,8 @@ func main() {
 	routes := route.NewRoute(actions)
 	certificates := initialization.ConfigService.Certificates
 	logrus.Info(certificates)
+	logrus.Info(certificates.CertificatesPath)
+	logrus.Info(certificates.KeyPath)
 	go run(serverInstance, routes, &initialization.ConfigService.Server, certificates.CertificatesPath, certificates.KeyPath)
 	stop()
 	serverInstance.Stop(context.Background(), businessDatabase)
