d76b4aa837a7f1a1f67dc16857d8064d24ba1e5e
konovalov07a24ve <vladkonovalov20060@gmail.com>
2025-08-03 18:12:55 +0500

    Просто для запуска

--- migrations/20250608091915_init.sql
+++ migrations/20250608091915_init.sql
@@ -3,24 +3,24 @@
 
 CREATE TABLE "File" (
    id serial PRIMARY KEY ,
-   file_id varchar,
-   name varchar NOT NULL,
-   mime_type varchar NOT NULL
+   file_id varchar(20),
+   name varchar(255) NOT NULL,
+   mime_type varchar(50) NOT NULL
 );
 
 CREATE TABLE "Session" (
-  session varchar NOT NULL,
+  session varchar(50) NOT NULL,
   file_id int NOT NULL,
   PRIMARY KEY (session, file_id)
 );
 
 CREATE TABLE "File_Parameters" (
-  session varchar NOT NULL,
+  session varchar(50) NOT NULL,
   file_id int NOT NULL,
-  password varchar,
+  password varchar(50),
   date_deleted date,
   count_download int,
-  description varchar,
+  description text,
   PRIMARY KEY (session, file_id)
 );
 
